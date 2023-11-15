75179768c171d119ab53a5d9d037e972e93401dc
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-11-15 15:12:18 +0530

    feat: Add dynamic background color changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import githubImg from "./images/github.png";
 
 function App() {
+  const [color, setColor] = useState("olive");
   return (
-    <div>
-      <h1>saurabh</h1>
-    </div>
-  )
+    <>
+      <div
+        className="w-full h-screen duration-200 relative"
+        style={{ backgroundColor: color }}
+      >
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg  py-4  text-white font-extrabold ">
+          <h1 className="text-4xl text-center">React BG Changer</h1>
+        </div>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-lg">
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+              onClick={() => setColor("red")}
+            >
+              Red
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "green" }}
+              onClick={() => setColor("green")}
+            >
+              Green
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "blue" }}
+              onClick={() => setColor("blue")}
+            >
+              Blue
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "black" }}
+              onClick={() => setColor("black")}
+            >
+              Black
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "gray" }}
+              onClick={() => setColor("gray")}
+            >
+              Gray
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "purple" }}
+              onClick={() => setColor("purple")}
+            >
+              Purple
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-yellow shadow-lg"
+              style={{ backgroundColor: "yellow" }}
+              onClick={() => setColor("yellow")}
+            >
+              Yellow
+            </button>
+          </div>
+        </div>
+        <a
+          href="https://github.com/saurabhjaykar1603/bg-changer"
+          target="_blank"
+        >
+          <img
+            src={githubImg}
+            alt="github"
+            className="h-20 absolute bottom-10 right-20 shadow-inner"
+          />
+        </a>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
